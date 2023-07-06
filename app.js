9ed2ac496ee865b2be0471b9d84fceea70e57ba9
sham  pun <begasos1997@gmail.com>
2023-07-06 12:10:30 +0400

    Express
    изучаю основы

--- app.js
+++ app.js
@@ -1,17 +1,11 @@
-// подключение express
 const express = require('express');
-// создаем объект приложения
+
 const app = express();
-// определяем обработчик для маршрутов
-app.get('/', function (request, response) {
-    // отправляем ответ
-    response.send('<h2>Привет Express!</h2>');
+
+app.use(express.static(__dirname + '/public'));
+
+app.use('/', function (request, response) {
+    response.send('<h1>Главная страница</h1>');
 });
-app.get('/about', function (request, response) {
-    response.send('<h1>О сайте</h1>');
-});
-app.get('/contact', function (request, response) {
-    response.send('<h1>Контакты</h1>');
-});
-// начинаем прослушивать подключения на 3000 порту
+
 app.listen(3000);
